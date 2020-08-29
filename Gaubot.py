6e8b960c2f5eb38d721058cc8b315b275ad5b8ba
Stan Putrya <root.vagner@gmail.com>
2020-08-29 14:42:18 +0200

    Code refactoring

--- Gaubot.py
+++ Gaubot.py
@@ -1,134 +1,229 @@
-from telegram.ext import Updater
+
 import logging
-from telegram.ext import CommandHandler
+import traceback
 from functools import wraps
-from telegram.ext.dispatcher import run_async
-from telegram.ext import MessageHandler, Filters
 import os
-import psutil
 import pathlib
 import subprocess
 from os import listdir
-from os.path import isfile, join, basename
+from os.path import isfile, join, basename, splitext
 import filecmp
+from glob import glob
 from pathlib import Path
 from subprocess import call
-pid_gaus = "0"
-LIST_OF_ADMINS = [469473176]
-gausman_path = '/home/docent/gausman'
-TOKEN = 'Your Token'
-updater = Updater(token=TOKEN, use_context=True)
-dispatcher = updater.dispatcher
-updater.start_polling()
+import pickle
+from multiprocessing.pool import ThreadPool
+from abc import ABC
+import gzip
+import shutil
+import argparse
+import yaml
 
+from telegram.ext import Updater, CommandHandler, Filters
+from telegram import Update, MessageEntity
+from telegram.ext.dispatcher import run_async
 
-# Security
-def restricted(func):
-    @wraps(func)
-    def wrapped(update, context, *args, **kwargs):
+logger = logging.getLogger('GauBot')
+
+class Wrapper(ABC):
+    _handlers = {}
+
+    @classmethod
+    def command(cls, name):
+        def wrapper(f):
+            cls._handlers[name] = f
+            @wraps(f)
+            def wrapped(self, *f_args, **f_kwargs):
+                f(self, *f_args, **f_kwargs)
+            return wrapped
+        return wrapper
+    
+    @classmethod
+    def access(cls, name):
+        def wrapper(f):
+            @wraps(f)
+            def wrapped(self, update, *f_args, **f_kwargs):
+                if not (name == "everyone" or self._check_access(name, update)):
+                    return
+                f(self, update, *f_args, **f_kwargs)
+            return wrapped
+        return wrapper
+
+class GauBot(CommandHandler, ABC):
+    def __init__(self, path, **kwargs):
+        self.wd = path
+        self.users_groups = kwargs.get("users", {})
+        self.pass_update_queue = False
+        self.pass_job_queue = False
+        self.pass_user_data = False
+        self.pass_chat_data = False
+        self._stop_flag = False
+        self._work_started = False
+        self._current_process = None
+        self.filters = Filters.update.messages
+
+    def _check_access(self, group_name, update):
         user_id = update.effective_user.id
-        if user_id not in LIST_OF_ADMINS:
-            print("Unauthorized access denied for {}.".format(user_id))
-            return
-        return func(update, context, *args, **kwargs)
-    return wrapped
+        if user_id not in self.users_groups[group_name]:
+            raise Exception("Unauthorized access denied for {}.".format(update.effective_user.name))
+            return False
+        return True
+
+    def check_update(self, update):
+        if isinstance(update, Update) and update.effective_message:
+            message = update.effective_message
+
+            if (message.entities and message.entities[0].type == MessageEntity.BOT_COMMAND
+                    and message.entities[0].offset == 0):
+                command = message.text[1:message.entities[0].length]
+                args = message.text.split()[1:]
+                command = command.split('@')
+                command.append(message.bot.username)
+
+                if not (command[0].lower() in Wrapper._handlers
+                        and command[1].lower() == message.bot.username.lower()):
+                    return None
+
+                filter_result = self.filters(update)
+                if filter_result:
+                    return args, filter_result, command[0]
+                else:
+                    return False
+
+    def handle_update(self, update, dispatcher, check_result, context=None):
+        args, filter_result, command = check_result
+        callback = Wrapper._handlers[command]
+        if context:
+            self.collect_additional_context(context, update, dispatcher, (args, filter_result))
+            try:
+                return callback(self, update, context)
+            except Exception as e:
+                context.bot.send_message(chat_id=update.effective_chat.id, text=f"Error: {str(e)}")
+                logging.error(e)
+                traceback.print_exc()
+        else:
+            optional_args = self.collect_optional_args(dispatcher, update, (args, filter_result))
+            return callback(self, dispatcher.bot, update, **optional_args)
 
 
-logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    def _run(self, cmd):
+        if self._stop_flag:
+            self._stop_flag = False
+            raise Exception("Fuck it!")
+        _current_process = subprocess.Popen(cmd, cwd=self.wd)
+        _current_process.wait()
+        return _current_process.returncode
+
+    def _compress(self, _in, _out):
+        with open(_in, 'rb') as f_in:
+            with gzip.open(_out, 'wb') as f_out:
+                shutil.copyfileobj(f_in, f_out)
+    
+    @Wrapper.command("help")
+    @Wrapper.access("everyone")
+    def help(self, update, context):
+        help_page = '/work <Имя файла> без расширения - поставить в очередь задачу; /init - запустить кластер расчётов; ' \
+                    '/stop - остановить кластер; /remove <Имя файла> без расширения - удалить файл из очереди; /list - ' \
+                    'вывести список файлов в очереди; /queue - вывести список файлов на сервере'
+        context.bot.send_message(chat_id=update.effective_chat.id, text=help_page)
+    
+    @Wrapper.command("start")
+    @Wrapper.access("admins")
+    def start(self, update, context):
+        context.bot.send_message(chat_id=update.effective_chat.id, text="Список команд /help")
+
+    @Wrapper.command("work")
+    @Wrapper.access("admins")
+    def work(self, update, context):
+        filename = str(update.message.text).split(' ')[1]
+        if filename == 'all':
+            subprocess.call(["rclone", "copy", "remote:Gbot/input/", self.wd])
+            file_list = os.listdir(self.wd)
+            context.bot.send_message(chat_id=update.effective_chat.id, text=str(['обрабатываются файлы', file_list]))
+        else:
+            context.bot.send_message(chat_id=update.effective_chat.id, text=str(['обрабатываются файлы', filename]))
+            subprocess.call(["rclone", "copy", "".join(["remote:Gbot/input/", filename, ".gjf"]), self.wd])
+    
+    @Wrapper.command("stop")
+    @Wrapper.access("admins")
+    def stop_work(self, update, context):
+        context.bot.send_message(chat_id=update.effective_chat.id, text='Прерывание работы кластера')
+        self._stop_flag = True
+        if self._current_process:
+            self._current_process.terminate()
+
+    @Wrapper.command("list")
+    @Wrapper.access("admins")
+    def list_work(self, update, context):
+        file_list = os.listdir(self.wd)
+        context.bot.send_message(chat_id=update.effective_chat.id, text=file_list)
+
+    @Wrapper.command("queue")
+    @Wrapper.access("admins")
+    def queue_work(self, update, context):
+        queue = str(subprocess.check_output(["rclone", "ls", "remote:Gbot/input"]))
+        context.bot.send_message(chat_id=update.effective_chat.id, text=queue)
+
+    @Wrapper.command("remove")
+    @Wrapper.access("admins")
+    def remove_work(self, update, context):
+        filename = str(update.message.text).split(' ')[1]
+        subprocess.call(["rm", "".join([filename, ".gjf"])])
+        context.bot.send_message(chat_id=update.effective_chat.id, text=['Удалена задача', filename])
+
+    @Wrapper.command("init")
+    @Wrapper.access("admins")
+    @run_async
+    def initiate(self, update, context):
+        if self._work_started:
+            raise Exception("I'm already doin' calc")
+        self._stop_flag = False
+        self._work_started = True
+        added = []
+        for f in glob(f"{self.wd}/*.gjf"):
+            logging.info(f"File processing: {f}")
+            context.bot.send_message(chat_id=update.effective_chat.id, text=f"File processing: {f}")
+            try:
+                ret_code = _run(["g16", f])
+            except:
+                logging.warning("Someone has killed us O_o")
+                break
+            if ret_code == 0:
+                try:
+                    f_zip = splitext(f)[0] + ".gz"
+                    _compress(join(self.wd, splitext(f)[0] + ".log"))
+                    _run(["rclone", "copy", join(self.wd, f_zip), "remote:Gbot/log/"]) # gapi
+                except:
+                    logging.warning("Someone has killed us O_o")
+                    break
+                os.remove(f)
+                context.bot.send_message(chat_id=update.effective_chat.id, text=f"File successefuly complited: {f}")
+                continue
+            try:
+                f_zip = splitext(f)[0] + ".gz"
+                _compress(join(self.wd, splitext(f)[0] + ".log"))
+                _run(["rclone", "copy", join(self.wd, f_zip), "remote:Gbot/error/"]) # gapi
+            except:
+                logging.warning("Someone has killed us O_o")
+                break
+            os.remove(f)
+            context.bot.send_message(chat_id=update.effective_chat.id, text=f"We're fucked up: {f}")
+        _work_started = False
+
+if __name__ == "__main__":
+    logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
                     level=logging.INFO)
+    parser = argparse.ArgumentParser(description='Gaussian telegram bot')
+    parser.add_argument('--config', type=str, default='config.ini')
+    args = parser.parse_args()
 
-
-# Запуск бота
-@restricted
-def start(update, context):
-    context.bot.send_message(chat_id=update.effective_chat.id, text="Список команд /help")
-
-
-start_handler = CommandHandler('start', start)
-dispatcher.add_handler(start_handler)
-
-
-@restricted
-def help(update, context):
-    help_page = '/work <Имя файла> без расширения - поставить в очередь задачу; /init - запустить кластер расчётов; ' \
-                '/stop - остановить кластер; /remove <Имя файла> без расширения - удалить файл из очереди; /list - ' \
-                'вывести список файлов в очереди; /queue - вывести список файлов на сервере'
-    context.bot.send_message(chat_id=update.effective_chat.id, text=help_page)
-
-
-help_handler = CommandHandler('help', help)
-dispatcher.add_handler(help_handler)
-
-
-@restricted
-def work(update, context):
-    filename = str(update.message.text).split(' ')[1]
-    if filename == 'all':
-        subprocess.call(["rclone", "copy", "remote:Gbot/input/", gausman_path])
-        file_list = os.listdir(gausman_path)
-        context.bot.send_message(chat_id=update.effective_chat.id, text=str(['обрабатываются файлы', file_list]))
-    else:
-        context.bot.send_message(chat_id=update.effective_chat.id, text=str(['обрабатываются файлы', filename]))
-        subprocess.call(["rclone", "copy", "".join(["remote:Gbot/input/", filename, ".gjf"]), gausman_path])
-
-
-work_handler = CommandHandler('work', work)
-dispatcher.add_handler(work_handler)
-
-
-@restricted
-@run_async
-def initiate(update, context):
-    global pid_gaus
-    pid_gaus = subprocess.Popen(["/usr/bin/python3.8", "gmanager.py"]).pid
-    context.bot.send_message(chat_id=update.effective_chat.id, text='Запущены расчёты. PID = {}'.format(pid_gaus))
-
-
-initiate_handler = CommandHandler('init', initiate)
-dispatcher.add_handler(initiate_handler)
-
-
-@restricted
-def stop_work(update, context):
-    context.bot.send_message(chat_id=update.effective_chat.id, text='Прерывание работы '
-                                                                    'кластера, PID={}'.format(pid_gaus))
-    process = psutil.Process(pid_gaus)
-    for proc in process.children(recursive=True):
-        proc.kill()
-    process.kill()
-
-
-stop_work_handler = CommandHandler('stop', stop_work)
-dispatcher.add_handler(stop_work_handler)
-
-
-@restricted
-def list_work(update, context):
-    file_list = os.listdir(gausman_path)
-    context.bot.send_message(chat_id=update.effective_chat.id, text=file_list)
-
-
-list_work_handler = CommandHandler('list', list_work)
-dispatcher.add_handler(list_work_handler)
-
-@restricted
-def queue_work(update, context):
-
-    queue = str(subprocess.check_output(["rclone", "ls", "remote:Gbot/input"]))
-
-    context.bot.send_message(chat_id=update.effective_chat.id, text=queue)
-
-
-queue_work_handler = CommandHandler('queue', queue_work)
-dispatcher.add_handler(queue_work_handler)
-
-
-@restricted
-def remove_work(update, context):
-    filename = str(update.message.text).split(' ')[1]
-    subprocess.call(["rm", "".join([filename, ".gjf"])])
-    context.bot.send_message(chat_id=update.effective_chat.id, text=['Удалена задача', filename])
-
-
-remove_work_handler = CommandHandler('remove', remove_work)
-dispatcher.add_handler(remove_work_handler)
+    with open(args.config, 'r') as cf:
+        cfg = yaml.load(open(args.config, 'r'), Loader=yaml.FullLoader)
+    updater = Updater(token=cfg['telegram']['token'], use_context=True)
+    updater.dispatcher.add_handler(
+        GauBot(
+            cfg['gaussian']['path'],
+            users=cfg['telegram']['groups']
+        )
+    )
+    updater.start_polling()
